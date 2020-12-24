3674b2cf18fcba8479e69e0874ff57892fd848cc
richierh <pmc.computer@yahoo.com>
2020-12-24 11:15:57 +0700

    okay

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from kivymd.app import MDApp
-from kivy.lang import Builder
-import kivy
-from kivy_garden.zbarcam import ZBarCam
+__version__ = '1.0'
 
+import kivy
+
+# importing file from https://github.com/kivy/plyer/blob/master/plyer/platforms/android/camera.py
+# I downloaded it and saved it in the same directory:
+from camera import AndroidCamera
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty, StringProperty
+
+import base64
 from android.permissions import request_permissions, Permission
 
 request_permissions([
@@ -10,28 +19,42 @@ request_permissions([
     Permission.WRITE_EXTERNAL_STORAGE,
     Permission.READ_EXTERNAL_STORAGE
 ])
-#:import ZBarCam kivy_garden.zbarcam.ZBarCam
 
-kv = """
-BoxLayout:
-    orientation: 'vertical'
-    ZBarCam:
-        id: zbarcam
-        # optional, by default checks all types
-        code_types: 'QRCODE', 'EAN13'
-    Label:
-        size_hint: None, None
-        size: self.texture_size[0], 50
-        text: ', '.join([str(symbol.data) for symbol in zbarcam.symbols])
-"""
+class MyCamera(AndroidCamera):
+    pass
 
-class RunApp(MDApp):
+class BoxLayoutW(BoxLayout):
+    my_camera = ObjectProperty(None)
+    # /sdcard means internal mobile storage for that case:
+    image_path = StringProperty('/sdcard/my_test_photo.png')
 
+    def __init__(self, **kwargs):
 
-    def build(self):
+        super(BoxLayoutW, self).__init__()
 
-        return Builder.load_string(kv)
+        self.my_camera = MyCamera()
 
+    def take_shot(self):
+        self.my_camera._take_picture(self.on_success_shot, self.image_path)
 
-if __name__=="__main__":
-    RunApp().run()
+    def on_success_shot(self, loaded_image_path):
+        # converting saved image to a base64 string:
+        image_str = self.image_convert_base64
+        return True
+
+    #converting image to a base64, if you want to send it, for example, via POST:
+    def image_convert_base64(self):
+        with open(self.image_path, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+        if not encoded_string:
+            encoded_string = ''
+        return encoded_string
+
+if __name__ == '__main__':
+
+    class CameraApp(App):
+        def build(self):
+            main_window = BoxLayoutW()
+            return main_window
+
+    CameraApp().run()
